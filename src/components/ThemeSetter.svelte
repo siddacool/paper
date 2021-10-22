6dfb5ad20c4324444485d6200d42898bb56b0b35
Siddhesh Mangela <sidthecool007@gmail.com>
2021-10-22 11:55:32 +0530

    Removed dark favicon

--- src/components/ThemeSetter.svelte
+++ src/components/ThemeSetter.svelte
@@ -4,21 +4,17 @@
   import theme from '~/store/theme';
 
   let bodyTag = null;
-  let faviconTag = null;
 
   if (browser) {
     bodyTag = document.querySelector('body');
-    faviconTag = document.querySelector('link[rel="icon"]');
   }
 
   const unsubscribe = theme.subscribe((value) => {
     if (browser) {
       if (value === 'dark') {
         bodyTag.classList.add('dark');
-        faviconTag.href = '/favicon-dark.svg';
       } else {
         bodyTag.classList.remove('dark');
-        faviconTag.href = '/favicon.svg';
       }
     }
   });
